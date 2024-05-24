765c18c141509654c75e32023a23483bd21e882c
domeepc <domagoj.milardovic@gmail.com>
2024-05-24 22:04:09 +0200

    removed idtoken from all providers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,18 +12,15 @@ export default NextAuth({
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			idToken: true,
 		}),
 		FacebookProvider({
 			clientId: process.env.FACEBOOK_CLIENT_ID,
 			clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-			idToken: true,
 		}),
 		TwitterProvider({
 			clientId: process.env.TWITTER_ID,
 			clientSecret: process.env.TWITTER_SECRET,
 			version: "2.0", // opt-in to Twitter OAuth 2.0
-			idToken: true,
 		}),
 		CredencialsProvider({
 			name: "Credencials",
